3daf17d28ac750b62489a2f57f447e3d33a38c35
Rushali <rushalisarkar2000@gmail.com>
2021-08-22 12:15:47 +0530

    Added password validation

--- validation-book/src/Main.hs
+++ validation-book/src/Main.hs
@@ -1,5 +1,38 @@
 module Main where
 
+import Data.Char
+
+checkPasswordLength :: String -> Maybe String
+checkPasswordLength password = 
+  case (length password > 20) of
+    True -> Nothing
+    False -> Just password
+
+requireAlphaNum :: String -> Maybe String
+requireAlphaNum xs = 
+  case (all isAlpha xs) of
+    False -> Nothing
+    True -> Just xs
+
+cleanWhiteSpace :: String -> Maybe String
+cleanWhiteSpace "" = Nothing
+cleanWhiteSpace (x:xs) = 
+  case (isSpace x) of
+    True -> cleanWhiteSpace xs
+    False -> Just (x:xs)
+
+
+validatePassword :: String -> String
+validatePassword password = 
+  case (checkPasswordLength password) of
+    Nothing -> "The password is more than 20 characters long"
+    Just password -> 
+      case (requireAlphaNum password) of
+        Nothing -> "The password cannot contain any specail character other than alphabets"
+        Just password -> "The password is valid"
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStr "Please enter password\n>"
+  password <- getLine
+  print $ validatePassword password
